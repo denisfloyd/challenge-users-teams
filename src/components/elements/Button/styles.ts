e8316ca519c4e1fba25db07e3ca753eef4761368
denismend <denisladeira1@gmail.com>
2022-04-01 08:59:23 -0300

    refactor: :lipstick: button comp style

--- src/components/elements/Button/styles.ts
+++ src/components/elements/Button/styles.ts
@@ -5,11 +5,17 @@ import { FONT_WEIGHT, SIZE } from "@/styles/abstracts/_variables";
 export const Container = styled.button`
   background: var(--blue-300);
   height: 3rem;
+
+  /* width: fit-content; */
+  display: flex;
+  align-items: center;
+  gap: ${SIZE._8};
+  margin-bottom: ${SIZE._16};
+
   border-radius: 5px;
   border: 0;
   padding: 0 ${SIZE._16};
   color: var(--white);
-  width: 100%;
   font-weight: ${FONT_WEIGHT.MEDIUM};
   margin-top: ${SIZE._16};
   transition: background-color 0.2s;
