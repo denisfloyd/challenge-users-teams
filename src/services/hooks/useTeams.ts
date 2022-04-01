e1cc712330d3b82841f1935a97a9b3b160fa9ef4
denismend <denisladeira1@gmail.com>
2022-03-31 22:15:34 -0300

    feat: :sparkles: useTeams react query hook

---
+++ src/services/hooks/useTeams.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "react-query";
+import { api } from "../apiClient";
+
+export type Team = {
+  id: string;
+  name: string;
+  teamLeadId: string;
+  teamMemberIds: string[];
+};
+
+export async function getTeams(): Promise<Team[] | null> {
+  try {
+    const { data } = await api.get("teams");
+    return data as Team[];
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function getTeam(id: string): Promise<Team | null> {
+  try {
+    const { data } = await api.get(`/teams/${id}`);
+    return data as Team;
+  } catch (e) {
+    return null;
+  }
+}
+
+export function useTeams(dragons?: Team[]) {
+  return useQuery(["dragons"], () => getTeams(), {
+    staleTime: 1000 * 60 * 1, // 1 minutes
+    refetchOnWindowFocus: true,
+    initialData: dragons,
+  });
+}
